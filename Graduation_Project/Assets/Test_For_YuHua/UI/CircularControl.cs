79f19d472d28eaed26a820e0fee6e23ccfb8fbbc
DanDanma <d0713227@mail.fcu.edu.tw>
2021-09-29 17:19:07 +0800

    touchpad adding ui and get touchpad position
    add ui activate
    add touchpad position detacting

---
+++ Graduation_Project/Assets/Test_For_YuHua/UI/CircularControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircularControl : MonoBehaviour
+{
+    public GameObject[] dataArr;
+    public string[] dataString;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

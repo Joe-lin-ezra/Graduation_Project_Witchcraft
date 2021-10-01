4d4a1d825fdf9f76e28bd3b3077a0f49cdd835ee
Siganmorfu <d0713227@mail.fcu.edu.tw>
2021-10-01 17:08:43 +0800

    ui script add

--- Graduation_Project/Assets/Test_For_YuHua/UI/CircularControl.cs
+++ Graduation_Project/Assets/Test_For_YuHua/UI/CircularControl.cs
@@ -10,8 +10,8 @@ public class CircularControl : MonoBehaviour
 
     public Vector2 pointPos;
     public Image pointer;
-    private float scalex = 0.2f;
-    private float scaley = 0.02f;
+    private float scalex = 0.5f;
+    private float scaley = 0.5f;
 
     // Start is called before the first frame update
     
@@ -23,6 +23,10 @@ public class CircularControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        pointer.transform.localPosition = new Vector3(pointPos.x * gameObject.transform.localPosition.x ,pointPos.y * gameObject.transform.localPosition.y,gameObject.transform.localPosition.z);
+        float trX = pointPos.x * scalex;
+        float trY = pointPos.y * scaley;
+        pointer.transform.localPosition = new Vector3(trX, trY, gameObject.transform.localPosition.z);
+        Debug.Log(string.Format("Local: X {0:0.00} ,Y {0:0.00}  Globe: X {0:0.00} ,Y {0:0.00}", pointer.transform.localPosition.x, pointer.transform.localPosition.y));
+        
     }
 }

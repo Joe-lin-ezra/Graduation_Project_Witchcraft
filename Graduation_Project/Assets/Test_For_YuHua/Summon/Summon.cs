ac85b4695721545ef914b4333df46f24038e07f4
yuhua <hu881121@gmail.com>
2021-10-19 05:12:19 +0800

    add text mesh and hp canvas

--- Graduation_Project/Assets/Test_For_YuHua/Summon/Summon.cs
+++ Graduation_Project/Assets/Test_For_YuHua/Summon/Summon.cs
@@ -17,7 +17,7 @@ public class Summon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-          
+        drawCircle();
     }
 
     // Update is called once per frame
--- Graduation_Project/Assets/Test_For_YuHua/UI/Circle/Script/CircularControl.cs
+++ Graduation_Project/Assets/Test_For_YuHua/UI/Circle/Script/CircularControl.cs
@@ -18,7 +18,8 @@ public class CircularControl : MonoBehaviour
     public int amount;
     public GameObject Canvas_DrawElement;
     public GameObject panel;
-    
+
+    public Texture[] monsterTexture;
     void Start()
     {
         
@@ -42,9 +43,7 @@ public class CircularControl : MonoBehaviour
         {
             Vector3 loc = RotateRound(new Vector3(0.2f,0,0), new Vector3(0,0),new Vector3(0f,1f,-0.3f),angle * i);
             Quaternion qua = new Quaternion(0, 0, 90, 0);
-            GameObject a =  Instantiate(element, panel.transform.position + (loc * 0.4f), qua);
-            a.GetComponent<Element>().selection = i;
-            
+            GameObject a =  Instantiate(element, panel.transform.position + (loc * 0.4f), qua);           
             //Create
 
             a.transform.rotation = Quaternion.Euler(75, 0, 0);
@@ -55,6 +54,11 @@ public class CircularControl : MonoBehaviour
 
             //Scle
             a.transform.SetParent(Canvas_DrawElement.GetComponentInChildren<Transform>());
+
+            //Element setting
+            Element tmp = a.GetComponent<Element>();
+            tmp.selection = i;
+            tmp.GetComponentInChildren<RawImage>().texture = monsterTexture[0];
         }
        
     }
@@ -66,6 +70,5 @@ public class CircularControl : MonoBehaviour
         Vector3 resultVec3 = center + point;
         return resultVec3;
     }
- 
     
 }

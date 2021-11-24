d3d61198d21c378871d74ce948893436abd160ee
DanDanMa <hu881121@gmail.com>
2021-11-24 14:02:15 +0800

    haha 我在雷

--- Graduation_Project/Assets/Test_For_YuHua/UI/Magic/MagicList.cs
+++ Graduation_Project/Assets/Test_For_YuHua/UI/Magic/MagicList.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class MagicList : MonoBehaviour
 {
-    public GameObject mgList;
+    public GameObject playerModel;
+    public Player playerC;
+    public GameObject[] mgList;
     public GameObject wordPrefab;
+    public TMP_Text mgicText;
+    public string text = "";
 
+    private void Awake()
+    {
+        playerC = playerModel.GetComponent<Player>();
+        mgList = playerC.MagicsOBJ;// array
+
+       
+        foreach (GameObject element in mgList)
+        {
+            text += string.Format("¡P {0} \n",element.name);
+        }
+        mgicText.text = text;
+    }
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
